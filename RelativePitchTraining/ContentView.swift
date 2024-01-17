3c834c172feae9cd48be3d0e34e4a1678f883f27
中北竜馬 <b1020183@fun.ac.jp>
2024-01-17 23:26:11 +0900

    Keyboardの鍵盤が押された時に、外部から渡された関数を呼び出すようにした

--- RelativePitchTraining/ContentView.swift
+++ RelativePitchTraining/ContentView.swift
@@ -1,22 +1,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var keys: [KeyState]
-
-    init() {
-        // 1オクターブ（0~12）の範囲での黒鍵のインデックス
-        let blacks: [Int] = [1, 3, 6, 8, 10]
-        // 12鍵盤 × 3オクターブ + ラスト1鍵盤
-        keys = (0 ..< 25).map { i in
-            // インデックスを12で割った余りで白鍵と黒鍵を判断
-            return KeyState(keyType: blacks.contains(i % 12) ? .black : .white)
-        }
-    }
-
     var body: some View {
         ZStack {
-            Color.black
-            KeyboardView(keys: $keys)
+            
         }
     }
 }
--- RelativePitchTraining/Feature/Keyboard/KeyboardView.swift
+++ RelativePitchTraining/Feature/Keyboard/KeyboardView.swift
@@ -1,7 +1,27 @@
+/*
+    参考
+    https://zenn.dev/kyome/articles/d6ec2e734291df
+*/
 import SwiftUI
 
 struct KeyboardView: View {
-    @Binding var keys: [KeyState]
+    private var keys: [KeyState]
+    private let onKeyTouched: (Int) -> Void
+    private let onKeyReleased: (Int) -> Void
+    
+    init(onKeyTouched: @escaping (Int) -> Void, onKeyReleased: @escaping (Int) -> Void) {
+        // 1オクターブ（0~12）の範囲での黒鍵のインデックス
+        let blacks: [Int] = [1, 3, 6, 8, 10]
+        // 12鍵盤 × 3オクターブ + ラスト1鍵盤
+        keys = (0 ..< 25).map { i in
+            // インデックスを12で割った余りで白鍵と黒鍵を判断
+            return KeyState(keyType: blacks.contains(i % 12) ? .black : .white)
+        }
+        
+        
+        self.onKeyTouched = onKeyTouched
+        self.onKeyReleased = onKeyReleased
+    }
 
     var body: some View {
         ZStack {
@@ -9,11 +29,19 @@ struct KeyboardView: View {
                 ForEach(Array(keys.enumerated()), id: \.element) { (offset, keyState) in
                     KeyShape(keyState: keyState)
                         .fill(keyState.color)
-            // 1鍵盤の高さを1とした時の幅を0.16として、その半分
-                        .aspectRatio(0.08, contentMode: .fit)
-                        // 黒鍵はzIndexで手前に描画する
-            .zIndex(keyState.zIndex)
-            // 黒鍵の歯抜けの箇所を補うためにダミーのViewを配置する
+                        .aspectRatio(0.08, contentMode: .fit) //1鍵盤の高さを1とした時の幅を0.16として、その半分
+                        .zIndex(keyState.zIndex) //黒鍵はzIndexで手前に描画する
+                        .onLongPressGesture(minimumDuration: .infinity, maximumDistance: .infinity, pressing: { isPressing in //TODO: これだと恐らくグリッサンドできない
+                            let note = offset + 48
+                            // タッチダウンとタッチアップを検出
+                            if isPressing {
+                                self.onKeyTouched(note)
+                            } else {
+                                self.onKeyReleased(note)
+                            }
+                        }, perform: {})
+
+                    // 黒鍵の歯抜けの箇所を補うためにダミーのViewを配置する
                     if [4, 11].contains(offset % 12) || offset == keys.count - 1 {
                         Rectangle()
                             .aspectRatio(0.08, contentMode: .fit)
@@ -24,5 +52,21 @@ struct KeyboardView: View {
         }
     // 3.52 = 0.16 × (7白鍵 × 3オクターブ + ラスト1鍵盤)
         .aspectRatio(3.52, contentMode: .fit)
+    }
+}
+
+struct KeyboardView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.black
+            KeyboardView(
+                onKeyTouched: { note in
+                    print("Key \(note) touched")
+                },
+                onKeyReleased: { note in
+                    print("Key \(note) released")
+                }
+            )
+        }
     }
 }
